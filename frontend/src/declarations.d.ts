27b946e55157c804cbe07ec498568b609bf1da88
Git-abby <malekabrar56@gmail.com>
2024-12-16 05:37:47 -0500

    fix: tailgard module fixed

---
+++ frontend/src/declarations.d.ts
@@ -0,0 +1,5 @@
+declare module "tailgrids/plugin" {
+    const plugin: any; // You can replace `any` with a more specific type if the plugin's API is documented.
+    export default plugin;
+  }
+  
